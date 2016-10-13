313ec6f6f4393a46f8c832a7da9f41825d666b86
Varun-Garg <varun.10@live.com>
2016-10-13 22:56:20 +0530

    insertion sort and heaps: insert, delete, update

--- HeapSort/src/Main.java
+++ HeapSort/src/Main.java
@@ -27,57 +27,126 @@ public class Main {
         HeapSort(arr, length - 1);
     }
 
+    public static void insert(int arr[], int value, int length)
+    {
+        arr[length+1] = value;
+        FixHeapPushUp(arr,length+1);
+    }
+
+    public static void delete(int arr[],int position, int length)
+    {
+        //as of now, works properly on head only
+
+        //we will be replacing it with last element of heap
+        //last element will be obviously less than parent
+        //so we only need to take care of lower elements
+
+        /*
+        int nextPosition;
+        for(nextPosition = position; nextPosition <= length; nextPosition *=2 );
+        if(nextPosition > length ) nextPosition /= 2;
+        if(nextPosition < length) nextPosition++; //right child */
+
+        arr[position] = arr[length];
+        FixHeapPushDown(arr,length-1,position);
+    }
+
+    public static void modify(int arr[],int position,int value, int length)
+    {
+        //we will be fixing heap both sides
+        arr[position] = value;
+        FixHeapPushDown(arr,length,position);
+        FixHeapPushUp(arr,position);
+    }
+
     public static void FixHeapPushDown(int arr[], int length, int top)
     //index begins at 1, so length is 1 greater than actual
     {
-        int c1 = top * 2;
-        if (c1 > length)
+        int child1Position = top * 2;
+        if (child1Position > length)
             return;
 
-        int c2 = top * 2 + 1;
+        int child2Position = top * 2 + 1;
         int temp;
 
         //FixHeapPushDown(arr, length, c1);
-        if ((c2) > length) //only first child exists
+        if ((child2Position) > length) //only first child exists
         {
-            if (arr[c1] > arr[top]) {
-                temp = arr[c1];
-                arr[c1] = arr[top];
+            if (arr[child1Position] > arr[top]) {
+                temp = arr[child1Position];
+                arr[child1Position] = arr[top];
                 arr[top] = temp;
-                FixHeapPushDown(arr, length, c1);
+                FixHeapPushDown(arr, length, child1Position);
             }
             return;
         }
-
         //FixHeapPushDown(arr, length, c2);// second child is also there
 
-        if (arr[c1] > arr[top] && arr[c1] > arr[c2]) {
-            temp = arr[c1];
-            arr[c1] = arr[top];
+        if (arr[child1Position] > arr[top] && arr[child1Position] > arr[child2Position]) {
+            temp = arr[child1Position];
+            arr[child1Position] = arr[top];
             arr[top] = temp;
-            FixHeapPushDown(arr, length, c1);
-        } else if (arr[c2] > arr[top]) {
-            temp = arr[c2];
-            arr[c2] = arr[top];
+            FixHeapPushDown(arr, length, child1Position);
+        } else if (arr[child2Position] > arr[top]) {
+            temp = arr[child2Position];
+            arr[child2Position] = arr[top];
             arr[top] = temp;
-            FixHeapPushDown(arr, length, c2);
+            FixHeapPushDown(arr, length, child2Position);
+        }
+    }
+
+    public static void FixHeapPushUp(int arr[], int currentPosition)
+    {
+        //index begins at 1, so length is 1 greater than actual
+        int parentPosition = currentPosition / 2;
+        if(parentPosition < 1)
+            return;
+
+        if(arr[currentPosition] > arr[parentPosition])
+        {
+            int temp;
+            temp = arr[currentPosition];
+            arr[currentPosition] = arr[parentPosition];
+            arr[parentPosition] = temp;
+            FixHeapPushUp(arr,parentPosition);
         }
     }
 
     public static void main(String[] args) throws IOException {
         System.out.print("Enter no of elements\t:");
-        int n;
+        int n,value,pos;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        int heap[] = new int[n + 1];
+        int heap[] = new int[n + 10 + 1]; // 10 extra spaces + 1 for index at 1
         for (int i = 1; i <= n; i++) {
             System.out.print("Enter " + i + "th element\t:");
             heap[i] = Integer.parseInt(br.readLine());
         }
 
         BuildHeapBottomUp(heap, n);
-
         System.out.println("Built heap is " + Arrays.toString(heap));
+
+        System.out.println("Enter value you would like to insert");
+        value = Integer.parseInt(br.readLine());
+        insert(heap,value,n);
+        n++;
+        System.out.println("Heap after insertion is " + Arrays.toString(heap));
+
+
+        System.out.println("Enter index you would like to delete (from 1) ");
+        pos = Integer.parseInt(br.readLine());
+        delete(heap,pos,n);
+        heap[n] = 0;
+        n--;
+        System.out.println("Heap after deletion is " + Arrays.toString(heap));
+
+        System.out.println("Enter index you would like to modify");
+        pos = Integer.parseInt(br.readLine());
+        System.out.println("Enter new value ");
+        value = Integer.parseInt(br.readLine());
+        modify(heap,pos,value,n);
+        System.out.println("Heap after modification is " + Arrays.toString(heap));
+
         HeapSort(heap,n);
         System.out.println("Sorted heap is " + Arrays.toString(heap));
 
---
+++ InsertionSort/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void InsertionSort(int arr[], int length)
+    {
+        int i,j,temp,k;
+        for(i = 1; i < length; i ++)
+        {
+            for(j = 0; j < i; j ++)
+            {
+                if(arr[j] > arr[i])
+                    break;
+            }
+            temp = arr[i];
+            k = i;
+            while (k > j)
+            {
+                arr[k] = arr[k-1];
+                k--;
+            }
+            arr[k] = temp;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException{
+        
+        int arr[]= new int[20];
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 0; i<10;i++)
+        {
+            arr[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        InsertionSort(arr,10);
+        System.out.println("Sorted array is " + Arrays.toString(arr));
+
+    }
+}
