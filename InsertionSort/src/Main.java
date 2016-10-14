151a63ceb322c55a40ef71b4d7b63f086f418161
Varun-Garg <varun.10@live.com>
2016-10-14 18:16:12 +0530

    merge sort

--- InsertionSort/src/Main.java
+++ InsertionSort/src/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException{
-        
+
         int arr[]= new int[20];
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
---
+++ MergeSort/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void MergeSort(int arr[], int begin, int end) {
+        //both inclusive
+        if (begin == end)
+            return;
+        int mid = (begin + end) / 2;
+        MergeSort(arr, begin, mid);
+        MergeSort(arr, mid + 1, end);
+
+        int tempArr[] = new int[end - begin + 1];
+        int i = begin, j = mid + 1, k = 0;
+        while (k < (end - begin + 1))
+            tempArr[k++] = (j > end || ((i <= mid) && arr[i] <= arr[j])) ? arr[i++] : arr[j++];
+
+        for (i = begin, k = 0; i <= end; i++, k++)
+            arr[i] = tempArr[k];
+    }
+
+    public static void main(String[] args) throws IOException {
+        int arr[] = new int[20];
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+        MergeSort(arr, 0, 9);
+        System.out.println("Sorted array is " + Arrays.toString(arr));
+
+    }
+}
